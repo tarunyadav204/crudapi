02274b3b617db7a8df887fac8719e3aa66f2ee51
tarun1508 <tarun204yadav@gmail.com>
2024-05-02 02:05:19 +0530

    run build command

--- client/crud-app/src/Components/AddUser/Add.jsx
+++ client/crud-app/src/Components/AddUser/Add.jsx
@@ -4,65 +4,66 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios";
 import { toast } from 'react-toastify';
 const Add = () => {
-  
+
   const navigate = useNavigate();
-  const [userData,setdata] = useState({
-    fname : "",
-    lname : "",
-    email : "",
-    password : ""
+  const [userData, setdata] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: ""
   });
 
-  const handelChange = (e)=>{
-    setdata({...userData , [e.target.name] : e.target.value});
+  const handelChange = (e) => {
+    setdata({ ...userData, [e.target.name]: e.target.value });
   }
-  
-  const handleSubmit = async (e)=>{
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     //  JSON.stringify(userData);
-    try{
-      const response =  await axios.post('http://localhost:3000/api/create' , userData);        
+    try {
+      const response = await axios.post('http://localhost:3000/api/create', userData);
+      //const response = await axios.post(`${window.location.origin}/api/create`, userData);
       const data = await response.data;
       //toast.success(data.msg, { position: 'top-left' });    
-        navigate("/");
+      navigate("/");
     }
-    catch(err){
-          console.error('Error submitting form:', err);
-          alert('Error submitting form');
+    catch (err) {
+      console.error('Error submitting form:', err);
+      alert('Error submitting form');
     }
   }
 
-  
+
 
   return (
     <div className='addUser'>
-        <Link to={"/"}>Back</Link>
-        <h3>Add new User</h3>
-        <form className='addUserForm' onSubmit={handleSubmit}>
-            <div className="inputgroup">
-                <label htmlFor="fname">First Name : </label>
-                <input type="text" id='fname' name='fname' autoComplete='off' placeholder='First Name'  onChange={handelChange} />
-            </div>
+      <Link to={"/"}>Back</Link>
+      <h3>Add new User</h3>
+      <form className='addUserForm' onSubmit={handleSubmit}>
+        <div className="inputgroup">
+          <label htmlFor="fname">First Name : </label>
+          <input type="text" id='fname' name='fname' autoComplete='off' placeholder='First Name' onChange={handelChange} />
+        </div>
 
-            <div className="inputgroup">
-                <label htmlFor="lname">Last Name : </label>
-                <input type="text" id='lname' name='lname' autoComplete='off' placeholder='Last Name' onChange={handelChange}  />
-            </div>
+        <div className="inputgroup">
+          <label htmlFor="lname">Last Name : </label>
+          <input type="text" id='lname' name='lname' autoComplete='off' placeholder='Last Name' onChange={handelChange} />
+        </div>
 
-            <div className="inputgroup">
-                <label htmlFor="email">Email : </label>
-                <input type="email" id='email' name='email' autoComplete='off' placeholder='Email' onChange={handelChange}   />
-            </div>
+        <div className="inputgroup">
+          <label htmlFor="email">Email : </label>
+          <input type="email" id='email' name='email' autoComplete='off' placeholder='Email' onChange={handelChange} />
+        </div>
 
-            <div className="inputgroup">
-                <label htmlFor="password">Password : </label>
-                <input type="password" id='password' name='password' autoComplete='off' placeholder='Password' onChange={handelChange}  />
-            </div>
+        <div className="inputgroup">
+          <label htmlFor="password">Password : </label>
+          <input type="password" id='password' name='password' autoComplete='off' placeholder='Password' onChange={handelChange} />
+        </div>
 
-            <div className="inputgroup">
-              <button type='submit'>ADD USER</button>
-            </div>
-        </form>
+        <div className="inputgroup">
+          <button type='submit'>ADD USER</button>
+        </div>
+      </form>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import  express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import route from "./router/userRoute.js";
+import path from 'path';
 
-const app =express();
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
@@ -14,17 +15,24 @@ dotenv.config();
 const PORT = 3000;
 const URL = process.env.MONGOURL;
 
-mongoose.connect(URL).then(()=>{
+mongoose.connect(URL).then(() => {
     console.log("Database Connected Successfully");
 
-    
 
-}).catch(err =>{
+
+}).catch(err => {
     console.log(err);
 });
 
+app.use("/api", route)
 
-app.use("/api" , route)
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "/frontend/build")));
+app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+);
+
+
 
 
 app.listen(PORT, () => {
